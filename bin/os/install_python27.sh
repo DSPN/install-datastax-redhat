491da74224d9b52a5743d6447653621389f0de2b
Gilbert Lau <gmflau@gmail.com>
2016-08-14 23:15:35 -0700

    added comment about python 2.7 install not working yet

--- bin/os/install_python27.sh
+++ bin/os/install_python27.sh
@@ -4,9 +4,9 @@
 yum update ## optional
 yum install -y scl-utils
 yum install -y centos-release-scl-rh
-yum install -y python27
-scl enable python27 bash
-export PYTHONPATH="/usr/lib/python2.7/site-packages/":$PYTHONPATH
 
 # Still have problem running the command below coz repo does not contain python27
 yum install -y python27
+
+scl enable python27 bash
+export PYTHONPATH="/usr/lib/python2.7/site-packages/":$PYTHONPATH
--- bin/reach_install_dse.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-echo 'reach install-datastax-redhat' >> reached_install_dse.out
