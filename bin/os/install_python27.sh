25198716cdc827f678d6a65c822f9826190df39a
Gilbert Lau <gmflau@gmail.com>
2016-08-15 23:27:44 -0700

    add "sudo"

--- bin/os/install_python27.sh
+++ bin/os/install_python27.sh
@@ -12,17 +12,14 @@ yum -y install sqlite-devel
 
 # Now to download, compile and install Python 2.7
 cd /opt
-curl -o Python-2.7.11.tar.xz https://www.python.org/ftp/python/2.7.11/Python-2.7.11.tar.xz
-tar xf Python-2.7.11.tar.xz
-cd Python-2.7.11
-./configure --prefix=/usr/local
-make && make altinstall
+sudo curl -o Python-2.7.11.tar.xz https://www.python.org/ftp/python/2.7.11/Python-2.7.11.tar.xz
+sudo tar xf Python-2.7.11.tar.xz
+sudo cd Python-2.7.11
+sudo ./configure --prefix=/usr/local
+sudo make && make altinstall
 
 # Link Python 2.7 to /usr/bin/python
-ln -s /usr/local/bin/python2.7 /usr/local/bin/python
-
-# Still have problem running the command below coz repo does not contain python27
-yum install -y python27
+sudo ln -s /usr/local/bin/python2.7 /usr/local/bin/python
 
 #scl enable python27 bash
 #export PYTHONPATH="/usr/lib/python2.7/site-packages/":$PYTHONPATH
