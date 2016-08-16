f788bf2dceb7d724f80dd47df3de52b46d1b7130
Gilbert Lau <gmflau@gmail.com>
2016-08-15 23:44:49 -0700

    added "sudo"

--- bin/os/install_python27.sh
+++ bin/os/install_python27.sh
@@ -16,7 +16,8 @@ sudo curl -o Python-2.7.11.tar.xz https://www.python.org/ftp/python/2.7.11/Pytho
 sudo tar xf Python-2.7.11.tar.xz
 sudo cd Python-2.7.11
 sudo ./configure --prefix=/usr/local
-sudo make && make altinstall
+sudo make
+sudo make altinstall
 
 # Link Python 2.7 to /usr/bin/python
 sudo ln -s /usr/local/bin/python2.7 /usr/local/bin/python
