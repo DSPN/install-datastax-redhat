3a07f85dc7356182a7bbbc92fa33a429a7b2fff7
Gilbert Lau <gmflau@gmail.com>
2016-08-17 14:38:15 -0700

    make alt install

--- bin/os/install_python27.sh
+++ bin/os/install_python27.sh
@@ -17,18 +17,19 @@ yum -y install xz
 
 # Now to download, compile and install Python 2.7
 cd /opt
-curl -o Python-2.7.9.tar.xz https://www.python.org/ftp/python/2.7.9/Python-2.7.9.tar.xz
-unxz Python-2.7.9.tar.xz
-tar -xf Python-2.7.9.tar
-cd Python-2.7.9
+curl -o Python-2.7.12.tar.xz https://www.python.org/ftp/python/2.7.12/Python-2.7.12.tar.xz
+unxz Python-2.7.12.tar.xz
+tar -xf Python-2.7.12.tar
+cd Python-2.7.12
 ./configure --prefix=/usr/local
 make
-make install
+make altinstall
 
 # Link Python 2.7 to /usr/bin/python
-#sudo ln -s /usr/local/bin/python2.7 /usr/local/bin/python
+rm /usr/bin/python
+ln -s /usr/local/bin/python2.7 /usr/bin/python
 
-#scl enable python27 bash
-#export PYTHONPATH="/usr/lib/python2.7/site-packages/":$PYTHONPATH
+# export PYTHONPATH for 2.7
+export PYTHONPATH="/usr/local/lib/python2.7/site-packages/":$PYTHONPATH
 
 echo "leaving install_python27.sh"  >> install_python27.sh.out
