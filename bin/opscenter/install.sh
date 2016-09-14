a40155c6e002ef8823a3768c308617b0d6d53f77
benofben <ben.lackey@outlook.com>
2016-09-13 17:58:49 -0700

    clean up comments

--- bin/opscenter/install.sh
+++ bin/opscenter/install.sh
@@ -2,9 +2,6 @@
 cloud_type=$1
 echo "Installing OpsCenter"
 
-echo "Adding the DataStax repository"
-
-# are we, I don't see it here...
 # temporarily using @google.com DS academy account, need to create one for Oracle Compute Cloud (OCC)
 
 echo | tee -a /etc/yum.repos.d/datastax.repo
@@ -14,6 +11,5 @@ echo "baseurl=https://datastax%40google.com:8GdeeVT2s7zi@rpm.datastax.com/enterp
 echo "enabled=1" | tee -a /etc/yum.repos.d/datastax.repo
 echo "gpgcheck=0" | tee -a /etc/yum.repos.d/datastax.repo
 
-# Ready to install OpsCenter
 opscenter_version=6.0.2
 yum -y install opscenter-$opscenter_version
