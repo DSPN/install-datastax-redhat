0973cd8d90e6effa93136d4f2d1825269719c3b7
Gilbert Lau <gmflau@users.noreply.github.com>
2016-10-15 12:54:40 -0700

    Update install_java.sh

--- bin/os/install_java.sh
+++ bin/os/install_java.sh
@@ -2,6 +2,7 @@
 
 echo "Installing the Oracle Java 8 JDK"
 
+yum install -y wget
 cd ~
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jdk-8u60-linux-x64.rpm"
 yum -y localinstall jdk-8u60-linux-x64.rpm
