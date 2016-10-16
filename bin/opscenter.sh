161d954ed35940a94033e1da6e1f640840f39fa6
Gilbert Lau <gmflau@users.noreply.github.com>
2016-10-15 17:34:07 -0700

    OCC - Oracle Elastic Cloud needs to install python 2.7.x
    OCC supports OEL 6.x and it has python 2.5.x and DSE needs 2.7.x.

--- bin/opscenter.sh
+++ bin/opscenter.sh
@@ -11,7 +11,10 @@ echo cloud_type \'$cloud_type\'
 echo seed_node_ip \'$seed_node_ip\'
 
 ./os/install_java.sh
-./os/install_python27.sh
+# occ currently supports OEL 6.x and has python 2.5 only
+if [ "$cloud_type" == "occ" ]; then
+  ./os/install_python27.sh
+fi
 ./opscenter/install.sh $cloud_type
 
 ./opscenter/start.sh
