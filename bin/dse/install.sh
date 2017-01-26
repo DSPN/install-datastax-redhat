836968f6bcb554d8198fbbff3acfdd6eb37f539d
Gilbert Lau <gmflau@users.noreply.github.com>
2017-01-25 23:59:07 -0800

    Update install.sh

--- bin/dse/install.sh
+++ bin/dse/install.sh
@@ -18,11 +18,11 @@ echo "gpgcheck=0" | tee -a /etc/yum.repos.d/datastax.repo
 rpm --import http://rpm.datastax.com/rpm/repo_key
 
 # Install the DSE package
-dse_version=5.0.5
+dse_version=5.0.3
 yum -y install dse-full-$dse_version dse-$dse_version dse-demos-$dse_version dse-libsolr-$dse_version dse-libtomcat-$dse_version dse-libsqoop-$dse_version dse-liblog4j-$dse_version dse-libmahout-$dse_version dse-libhadoop-native-$dse_version dse-libcassandra-$dse_version dse-libhive-$dse_version dse-libpig-$dse_version dse-libhadoop-$dse_version dse-libspark-$dse_version  dse-libgraph-$dse_version dse-libhadoop2-client-$dse_version
 
 echo "Running yum install datastax-agent"
-opscenter_version=6.0.7
+opscenter_version=6.0.3
 yum -y install datastax-agent-$opscenter_version
 
 # The install of dse creates a cassandra user, so now we can do this:
