3bc16d6df735b32ca741d738a655d90912f2b64d
Gilbert Lau <gmflau@users.noreply.github.com>
2017-05-02 16:01:19 -0700

    Update install.sh

--- bin/dse/install.sh
+++ bin/dse/install.sh
@@ -18,7 +18,7 @@ echo "gpgcheck=0" | tee -a /etc/yum.repos.d/datastax.repo
 rpm --import http://rpm.datastax.com/rpm/repo_key
 
 # Install the DSE package
-dse_version=5.1.0
+dse_version=5.1.0-1
 yum -y install dse-full-$dse_version dse-$dse_version dse-demos-$dse_version dse-libsolr-$dse_version dse-libtomcat-$dse_version dse-libsqoop-$dse_version dse-liblog4j-$dse_version dse-libmahout-$dse_version dse-libhadoop-native-$dse_version dse-libcassandra-$dse_version dse-libhive-$dse_version dse-libpig-$dse_version dse-libhadoop-$dse_version dse-libspark-$dse_version  dse-libgraph-$dse_version dse-libhadoop2-client-$dse_version
 
 echo "Running yum install datastax-agent"
