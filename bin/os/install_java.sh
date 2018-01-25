d02ed8837e8f562e3cf2bbca354e93d34fa9e6fc
Gilbert Lau <gmflau@gmail.com>
2018-01-25 11:28:39 -0800

    Update OPSC version and JDK 8u162

--- bin/os/install_java.sh
+++ bin/os/install_java.sh
@@ -4,5 +4,5 @@ echo "Installing the Oracle Java 8 JDK"
 
 yum install -y wget
 cd ~
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm"
-yum -y localinstall jdk-8u131-linux-x64.rpm
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u162-b12/0da788060d494f5095bf8624735fa2f1/jdk-8u162-linux-x64.rpm"
+yum -y localinstall jdk-8u162-linux-x64.rpm
