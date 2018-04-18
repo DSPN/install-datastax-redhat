0550866ced6c739605af7ce9e3f3fbad06463e75
Gilbert Lau <gmflau@gmail.com>
2018-04-18 08:09:23 -0700

    Bumped it up to jdk-8u172-linux-x64

--- bin/os/install_java.sh
+++ bin/os/install_java.sh
@@ -4,5 +4,5 @@ echo "Installing the Oracle Java 8 JDK"
 
 yum install -y wget
 cd ~
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u162-b12/0da788060d494f5095bf8624735fa2f1/jdk-8u162-linux-x64.rpm"
-yum -y localinstall jdk-8u162-linux-x64.rpm
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u172-b11/a58eab1ec242421181065cdc37240b08/jdk-8u172-linux-x64.rpm"
+yum -y localinstall jdk-8u172-linux-x64.rpm
