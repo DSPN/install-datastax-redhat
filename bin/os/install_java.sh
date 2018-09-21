470f75e32db15acaf7055eb351fe0f95d31d88e1
Gilbert Lau <gmflau@users.noreply.github.com>
2018-09-21 00:31:41 -0700

    Moved to OpenJDK 8

--- bin/os/install_java.sh
+++ bin/os/install_java.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Installing the Oracle Java 8 JDK"
+echo "Installing the OpenJDK 8"
 
-yum install -y wget
 cd ~
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u172-b11/a58eab1ec242421181065cdc37240b08/jdk-8u172-linux-x64.rpm"
-yum -y localinstall jdk-8u172-linux-x64.rpm
+yum -y install java-1.8.0-openjdk
